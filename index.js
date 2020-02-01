48bf332c688fb6314fedaf2df15a03ba155b14ca
SASBoom <balrajshah@live.com>
2020-02-01 18:54:41 -0500

    Add register route

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var userController = require('./controllers/users');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -6,6 +9,10 @@ const port = process.env.PORT || 3000;
 app.get("/", function(req, res) {
     res.send("Welcome to my Platform API: Version 1.0.0");
 });
+
+app.use(bodyParser.json());
+
+app.post("/register", userController.registerUser);
 
 app.listen(port, () => {
     console.log(`LISTENING ON ${port}`);
