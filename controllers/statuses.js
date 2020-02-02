2332d6934291e8bd629a648ec09c354d1a30c968
SASBoom <balrajshah@live.com>
2020-02-02 13:33:05 -0500

    Add history for all users controller fn and refactor a bit

--- controllers/statuses.js
+++ controllers/statuses.js
@@ -1,13 +1,21 @@
 const events = require('./events');
+const userRepo = require('../repositories/users');
 var jwt = require('jsonwebtoken');
 const R = require('ramda');
 
 const privateKey = process.env.privateKey || 'foobar';
 
-const newStatus = (req, res, next) => {
+//TODO: Move this fn to a utility file or part of route chain prior to hitting controller code
+const getUserFromToken = req => {
     const token = req.headers["authorization"].split(" ")[1];
     const decodedToken = jwt.verify(token, privateKey);
-    const data = R.omit(['iat'], decodedToken);
+    const user = R.omit(['iat'], decodedToken);
+    return user;
+}
+
+//TODO: Move parameter checks to a separate service file
+const newStatus = (req, res, next) => {
+    const user = getUserFromToken(req);
     let eventType;
     if (req.body["type"] === 'checkin') {
         eventType = 'USER_CHECKED_IN';
@@ -17,7 +25,7 @@ const newStatus = (req, res, next) => {
         res.status(400).send("Invalid status type");
         return next();
     }
-    return events.getLastEventForUser(data.id)
+    return events.getLastEventForUser(user.id)
     .then(lastEvent => {
         if (lastEvent === undefined) {
             res.status(404).send("No user activity found.");
@@ -34,21 +42,23 @@ const newStatus = (req, res, next) => {
                 return next();
             }
         }
-        return events.logEvent(eventType, data)
+        return events.logEvent(eventType, user)
         .then(event => {
             res.status(200).send(event);
+            return next();
+        })
+        .catch(err => {
+            res.status(500).send({error: err});
             return next();
         });
     });
 }
 
 const getStatusHistoryForUser = (req, res, next) => {
-    const token = req.headers["authorization"].split(" ")[1];
-    const decodedToken = jwt.verify(token, privateKey);
-    const data = R.omit(['iat'], decodedToken);
+    const user = getUserFromToken(req);
     const offset = req.query.offset;
     const limit = req.query.limit;
-    if (data.id !== req.params.user_id) {
+    if (user.id !== req.params.user_id) {
         res.status(403).send("You are not authorized to view this.");
     }
     return events.getEventsForUser(req.params.user_id, offset, limit)
@@ -56,9 +66,36 @@ const getStatusHistoryForUser = (req, res, next) => {
         res.status(200).send({events: events});
         return next();
     })
+    .catch(err => {
+        res.status(500).send({error: err});
+        return next();
+    });
+}
+
+const getStatusHistoryForAllUsers = (req, res, next) => {
+    const user = getUserFromToken(req);
+    const offset = req.query.offset;
+    const limit = req.query.limit;
+    return userRepo.getUserByUserId(user.id)
+    .then(user => {
+        if (!user) {
+            res.status(403).send("You are not authorized to view this.");
+        }
+        return user;
+    })
+    .then(() => events.getEventsForAllUsers(offset, limit))
+    .then(events => {
+        res.status(200).send({events: events});
+        return next();
+    })
+    .catch(err => {
+        res.status(500).send({error: err});
+        return next();
+    });
 }
 
 module.exports = {
     newStatus,
     getStatusHistoryForUser,
+    getStatusHistoryForAllUsers,
 }
