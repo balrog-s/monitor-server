08b1dbc06450750f098b8da08a7d965c8c1fa8a7
SASBoom <balrajshah@live.com>
2020-02-01 22:33:41 -0500

    Implement status controller

---
+++ controllers/status.js
@@ -0,0 +1,24 @@
+const eventLogger = require('./eventLogger');
+var {uuid} = require('uuidv4');
+
+const privateKey = process.env.privateKey || 'foobar';
+
+const checkIn = (req, res, next) => {
+    const token = req.headers.token.split(" ")[1];
+    const decodedToken = jwt.verify(token, privateKey);
+    console.log(decodedToken);
+    res.status(200).send({user: decodedToken});
+    return eventLogger.insertEvent({
+        id: uuid(),
+        event_type: 'USER_CHECKED_IN',
+        data: decodedToken
+    }).then(event => {
+        console.log(event);
+        res.status(200).send(event);
+        return next();
+    });
+}
+
+module.exports = {
+    checkIn
+}
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var cors = require('cors');
 
 var userController = require('./controllers/users');
+var statusController = require('./controllers/status');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,6 +17,7 @@ app.use(cors());
 
 app.post("/register", userController.registerUser);
 app.post("/login", userController.loginUser);
+app.post("/status", statusController.checkIn);
 
 app.listen(port, () => {
     console.log(`LISTENING ON ${port}`);
