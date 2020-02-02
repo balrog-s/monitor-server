8b7fc574f9dc865aac1a908ac78fb016da86ef62
SASBoom <balrajshah@live.com>
2020-02-01 22:52:57 -0500

    Fix checkIn controller

--- controllers/status.js
+++ controllers/status.js
@@ -1,19 +1,19 @@
 const eventLogger = require('./eventLogger');
 var {uuid} = require('uuidv4');
+var jwt = require('jsonwebtoken');
+const R = require('ramda');
 
 const privateKey = process.env.privateKey || 'foobar';
 
 const checkIn = (req, res, next) => {
-    const token = req.headers["Authorization"].split(" ")[1];
+    const token = req.headers["authorization"].split(" ")[1];
     const decodedToken = jwt.verify(token, privateKey);
-    console.log(decodedToken);
-    res.status(200).send({user: decodedToken});
-    return eventLogger.insertEvent({
+    const data = R.omit(['iat'], decodedToken);
+    return eventLogger.logEvent({
         id: uuid(),
         event_type: 'USER_CHECKED_IN',
-        data: decodedToken
+        data
     }).then(event => {
-        console.log(event);
         res.status(200).send(event);
         return next();
     });
