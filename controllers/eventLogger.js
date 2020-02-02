b7e41472db50c4c7049a5c06cf3d659118b77c4e
SASBoom <balrajshah@live.com>
2020-02-01 19:42:58 -0500

    Fix event object using camelcase instead of snakecase

--- controllers/eventLogger.js
+++ controllers/eventLogger.js
@@ -4,7 +4,7 @@ var eventsRepo = require('../repositories/events');
 const logEvent = (eventType, data) => {
     const event = {
         id: uuid(),
-        eventType,
+        event_type: eventType,
         data
     };
     return eventsRepo.insertEvent(event);
