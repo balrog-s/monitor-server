a2319cf8c279ef0b4c82d3fb7f1d676e54dbeb85
SASBoom <balrajshah@live.com>
2020-02-01 23:38:09 -0500

    Remove uniquedata column constraint and add status type query param

--- controllers/status.js
+++ controllers/status.js
@@ -9,7 +9,16 @@ const checkIn = (req, res, next) => {
     const token = req.headers["authorization"].split(" ")[1];
     const decodedToken = jwt.verify(token, privateKey);
     const data = R.omit(['iat'], decodedToken);
-    return eventLogger.logEvent("USER_CHECKED_IN", data).then(event => {
+    let eventType;
+    if (req.query["type"] === 'checkin') {
+        eventType = 'USER_CHECKED_IN';
+    } else if (req.query["type"] === 'checkout'){
+        eventType = "USER_CHECKED_OUT";
+    } else {
+        res.status(400).send("Invalid status type");
+        return next();
+    }
+    return eventLogger.logEvent(eventType, data).then(event => {
         res.status(200).send(event);
         return next();
     });
--- db/migrations/20200201191410_add_events_table.js
+++ db/migrations/20200201191410_add_events_table.js
@@ -7,7 +7,7 @@ exports.up = function(knex, Promise) {
         }
         return knex.schema.createTable('events', function (t) {
             t.uuid('id').notNull().unique();
-            t.jsonb('data').notNull().unique();
+            t.jsonb('data').notNull();
             t.enu('event_type', ['USER_REGISTERED', 'USER_LOGGED_IN', 'USER_CHECKED_IN', 'USER_CHECKED_OUT']);
             t.timestamps(null, true);
         });
