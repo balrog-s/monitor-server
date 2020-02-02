a7b478d421e966f71d9b6535ba676147423bf33c
SASBoom <balrajshah@live.com>
2020-02-01 21:38:28 -0500

    Add login route

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.post("/register", userController.registerUser);
+app.post("/login", userController.loginUser);
 
 app.listen(port, () => {
     console.log(`LISTENING ON ${port}`);
